68abd7c1e0fd4462aa5191906b7915df3bb6bdc7
foxzicool <rwerwerwerwtt@gmail.com>
2024-11-29 15:45:36 +0800

    ewrweeeerer555

--- stories/ThMenu.stories.ts
+++ stories/ThMenu.stories.ts
@@ -1,27 +1,34 @@
-import type { Meta, StoryFn } from '@storybook/vue3';
+import type { StoryFn } from '@storybook/vue3';
 import ThMenu from '~/components/ThMenu.vue';
 import type { ThMenuProps } from '~/components/types';
+import { icons } from '~/components/icon';
 
 export default {
   title: 'Components/ThMenu',
   component: ThMenu,
   argTypes: {
+    buttonLabel: {
+      control: 'text',
+      description: 'Label for the dropdown button',
+    },
+    icon: {
+      type: 'enum',
+      control: 'select',
+      description: 'Icon to use for the button',
+      options: icons,
+    },
     items: {
       control: 'object',
       description:
-        'Array of menu items with subitems, each item might include a "to" and "external" property for links.',
+        'Array of menu items with subitems, each item might include a "to", "external" and "icon" property for links and icons.',
     },
     location: {
       control: 'select',
       options: ['top', 'bottom', 'start', 'end', 'center'],
       description: 'Menu anchor location',
     },
-    buttonLabel: {
-      control: 'text',
-      description: 'Label for the dropdown button',
-    },
   },
-} as Meta<ThMenuProps>;
+};
 
 const Template: StoryFn<ThMenuProps> = (args) => ({
   components: { ThMenu },
@@ -38,19 +45,22 @@ Default.args = {
       title: 'Main Option 1',
       to: 'https://nuxt.com',
       external: false,
+      icon: 'menu',
       subItems: [
         {
           title: 'Suboption 1-1',
           to: 'https://nuxt.com',
           external: false,
+          icon: 'menu',
         },
         {
           title: 'Suboption 1-2',
           to: 'https://nuxt.com',
           external: true,
+          icon: 'menu',
           subItems: [
-            { title: 'Sub-suboption 1-2-1', to: 'https://nuxt.com', external: false },
-            { title: 'Sub-suboption 1-2-2', to: 'https://nuxt.com', external: true },
+            { title: 'Sub-suboption 1-2-1', to: 'https://nuxt.com', external: false, icon: 'menu' },
+            { title: 'Sub-suboption 1-2-2', to: 'https://nuxt.com', external: true, icon: 'menu' },
           ],
         },
       ],
@@ -59,20 +69,24 @@ Default.args = {
       title: 'Main Option 2',
       to: 'https://nuxt.com',
       external: true,
+      icon: 'menu',
       subItems: [
         {
           title: 'Suboption 2-1',
           to: 'https://nuxt.com',
           external: false,
+          icon: 'menu',
         },
         {
           title: 'Suboption 2-2',
           to: 'https://nuxt.com',
           external: false,
+          icon: 'menu',
         },
       ],
     },
   ],
   buttonLabel: '關於我們',
-  location: 'end',
+  location: 'top',
+  icon: 'arrow_right',
 };
