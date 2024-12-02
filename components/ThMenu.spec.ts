4cb31b79f2bcedfb4899cf9d4fbcc0aaa8078a0d
foxzicool <rwerwerwerwtt@gmail.com>
2024-12-02 14:28:52 +0800

    ewrwrrerer555

--- components/ThMenu.spec.ts
+++ components/ThMenu.spec.ts
@@ -9,22 +9,22 @@ describe('<ThMenu />', () => {
       title: 'Main Option 1',
       to: 'https://nuxt.com',
       external: false,
-      icon: 'menu',
+      icon: 'menu' as const,
       subItems: [
         {
           title: 'Suboption 1-1',
           to: 'https://nuxt.com',
           external: false,
-          icon: 'menu'
+          icon: 'menu' as const
         },
         {
           title: 'Suboption 1-2',
           to: 'https://nuxt.com',
           external: true,
-          icon: 'menu',
+          icon: 'menu' as const,
           subItems: [
-            { title: 'Sub-suboption 1-2-1', to: 'https://nuxt.com', external: false, icon: 'menu' },
-            { title: 'Sub-suboption 1-2-2', to: 'https://nuxt.com', external: true, icon: 'menu' }
+            { title: 'Sub-suboption 1-2-1', to: 'https://nuxt.com', external: false, icon: 'menu' as const },
+            { title: 'Sub-suboption 1-2-2', to: 'https://nuxt.com', external: true, icon: 'menu' as const }
           ]
         }
       ]
@@ -34,15 +34,36 @@ describe('<ThMenu />', () => {
   const props = {
     items,
     buttonLabel: 'About Us',
-    icon: 'arrow_right'
+    icon: 'arrow_right' as const
   };
 
-  it('renders the menu with the specified button label and icon', async () => {
+  it('renders button with label and icon', async () => {
     const wrapper = await mountSuspended(ThMenu, { props });
     expect(wrapper.text()).toContain(props.buttonLabel);
     expect(wrapper.findComponent({ name: 'ThIcon' }).exists()).toBe(true);
   });
-
+  it('renders all icons for menu items at every level', async () => {
+    const wrapper = await mountSuspended(ThMenu, { props });
+    items.forEach((item, i) => {
+      if (item.icon) {
+        expect(wrapper.findAll('.th-icon')[i].isVisible()).toBe(true);
+      }
+      if (item.subItems) {
+        item.subItems.forEach((subItem, j) => {
+          if (subItem.icon) {
+            expect(wrapper.findAll('.th-icon')[i + j + 1].isVisible()).toBe(true);
+          }
+          if (subItem.subItems) {
+            subItem.subItems.forEach((subSubItem, k) => {
+              if (subSubItem.icon) {
+                expect(wrapper.findAll('.th-icon')[i + j + k + 2].isVisible()).toBe(true);
+              }
+            });
+          }
+        });
+      }
+    });
+  });
   common.itMergesClass(ThMenu, { props });
   common.itMergesStyle(ThMenu, { props });
 });
