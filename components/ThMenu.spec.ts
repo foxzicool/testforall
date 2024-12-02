8d41394da1926e7631be5a1d4fbaf88630a001a0
foxzicool <rwerwerwerwtt@gmail.com>
2024-12-02 14:11:06 +0800

    ewrw444erer555

--- components/ThMenu.spec.ts
+++ components/ThMenu.spec.ts
@@ -9,22 +9,22 @@ describe('<ThMenu />', () => {
       title: 'Main Option 1',
       to: 'https://nuxt.com',
       external: false,
-      icon: 'menu' as const,
+      icon: 'menu',
       subItems: [
         {
           title: 'Suboption 1-1',
           to: 'https://nuxt.com',
           external: false,
-          icon: 'menu' as const
+          icon: 'menu'
         },
         {
           title: 'Suboption 1-2',
           to: 'https://nuxt.com',
           external: true,
-          icon: 'menu' as const,
+          icon: 'menu',
           subItems: [
-            { title: 'Sub-suboption 1-2-1', to: 'https://nuxt.com', external: false, icon: 'menu' as const },
-            { title: 'Sub-suboption 1-2-2', to: 'https://nuxt.com', external: true, icon: 'menu' as const }
+            { title: 'Sub-suboption 1-2-1', to: 'https://nuxt.com', external: false, icon: 'menu' },
+            { title: 'Sub-suboption 1-2-2', to: 'https://nuxt.com', external: true, icon: 'menu' }
           ]
         }
       ]
@@ -33,12 +33,11 @@ describe('<ThMenu />', () => {
 
   const props = {
     items,
-    location: 'top' as const,
     buttonLabel: 'About Us',
-    icon: 'arrow_right' as const
+    icon: 'arrow_right'
   };
 
-  it('renders button with label and icon', async () => {
+  it('renders the menu with the specified button label and icon', async () => {
     const wrapper = await mountSuspended(ThMenu, { props });
     expect(wrapper.text()).toContain(props.buttonLabel);
     expect(wrapper.findComponent({ name: 'ThIcon' }).exists()).toBe(true);
@@ -46,24 +45,4 @@ describe('<ThMenu />', () => {
 
   common.itMergesClass(ThMenu, { props });
   common.itMergesStyle(ThMenu, { props });
-
-  // 如果以后想要测试嵌套菜单项，可以取消下面的注释
-  /*
-  describe('nested menu items', () => {
-    it('renders all menu items with correct attributes and submenus', async () => {
-      const wrapper = await mountSuspended(ThMenu, { props });
-      const listItems = wrapper.findAll('.v-list-item');
-      expect(listItems.length).toBeGreaterThan(0);
-
-      listItems.forEach((item, index) => {
-        const flatItems = items.flatMap(i => [i].concat(i.subItems || []));
-        const expectedItem = flatItems[index];
-        expect(item.text()).toContain(expectedItem.title);
-        if (expectedItem.icon) {
-          expect(item.find('.th-icon').exists()).toBe(true);
-        }
-      });
-    });
-  });
-  */
 });
