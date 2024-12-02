a3360da9740068483873861c0eb98729483731ce
foxzicool <rwerwerwerwtt@gmail.com>
2024-12-02 14:39:08 +0800

    ewrwrrweewe4rler555

--- components/ThMenu.spec.ts
+++ components/ThMenu.spec.ts
@@ -43,33 +43,7 @@ describe('<ThMenu />', () => {
     expect(wrapper.text()).toContain(props.buttonLabel);
     expect(wrapper.findComponent({ name: 'ThIcon' }).exists()).toBe(true);
   });
-  it('renders all icons for menu items at every level', async () => {
-    const wrapper = await mountSuspended(ThMenu, { props });
-    const icons = wrapper.findAll('.th-icon');
-    expect(icons.length).toBeGreaterThan(0);
-  
-    let iconIndex = 0;
-    items.forEach((item) => {
-      if (item.icon) {
-        expect(icons[iconIndex].isVisible()).toBe(true);
-        iconIndex++;
-      }
-      item.subItems.forEach((subItem) => {
-        if (subItem.icon) {
-          expect(icons[iconIndex].isVisible()).toBe(true);
-          iconIndex++;
-        }
-        subItem.subItems.forEach((subSubItem) => {
-          if (subSubItem.icon) {
-            expect(icons[iconIndex].isVisible()).toBe(true);
-            iconIndex++;
-          }
-        });
-      });
-    });
-  });
-  
+
   common.itMergesClass(ThMenu, { props });
   common.itMergesStyle(ThMenu, { props });
-
 });
